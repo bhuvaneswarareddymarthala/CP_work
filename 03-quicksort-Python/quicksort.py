10ecc899a872cfaca014a1044a6bb40aaa961cc7
bhuvaneswarareddymarthala <70951053+bhuvaneswarareddymarthala@users.noreply.github.com>
2021-08-17 12:34:43 +0530

    gre

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -1,6 +1,32 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
+def part(arr, low, high):
+	i = (low-1)
+	pivot = arr[high]   
+	for j in range(low, high):
+  
+        # If current element is smaller than or
+        # equal to pivot
+		if arr[j] <= pivot:
+  
+            # increment index of smaller element
+			i = i+1
+			arr[i], arr[j] = arr[j], arr[i]
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return (i+1)
+
+
+def sort(arr, l ,h):
+	if len(arr) == 1:
+		return arr
+	if l<h:
+		p = part(arr, l , h)
+		sort(arr, l , p-1)
+		sort(arr,p,h)
+
 def quicksort(array):
 	# Your code goes here
-	pass
+	n = len(array)
+	sort(array, 0 , n-1)
+	return array
